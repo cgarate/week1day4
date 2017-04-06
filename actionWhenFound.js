95e7d5d8cf2ad84674441eb0878c876f16e8dc82
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-06 17:36:25 +0000

    Refactored code to use array.forEach method instead of for loop.

--- actionWhenFound.js
+++ actionWhenFound.js
@@ -1,14 +1,21 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+
+  arr.forEach( function (item, theIndex) {
+    if (item == "Waldo") {
+      found(theIndex);
     }
-  }
+  });
+
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
 }
 
 function actionWhenFound(theIndex) {
   console.log("Found him at index " + theIndex + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston", "Waldo"], actionWhenFound);
