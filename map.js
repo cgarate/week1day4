fe754e05bccff538ac42db06e77a15ce63f1d0b0
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-06 19:25:35 +0000

    Added base code to create a map function.

---
+++ map.js
@@ -0,0 +1,14 @@
+
+// Create your own version of a map function.
+// It should take two arguments, the array to map and a callback function.
+
+var map = function (array, f) {
+
+}
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
