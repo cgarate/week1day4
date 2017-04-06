319bbeb5d4aa4d975b1930fdbca4636425df426d
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-06 19:30:52 +0000

    Finished map.js

--- map.js
+++ map.js
@@ -3,12 +3,13 @@
 // It should take two arguments, the array to map and a callback function.
 
 var map = function (array, f) {
-
+  return array.map(f);
 }
 
 var words = ["ground", "control", "to", "major", "tom"];
 
-map(words, function(word) {
+var newArray = map(words, function(word) {
   return word.length;
 });
 
+console.log(newArray);
