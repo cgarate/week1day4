012b41b15cf913532d071dc4ba66d4281708da15
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-06 18:58:01 +0000

    Ordered a numeric array correctly using a compareFunction

---
+++ arraySort.js
@@ -0,0 +1,13 @@
+
+// Some examples of how to deal with array.sort()
+
+var array = [10, 2, 5, 1, 9];
+
+console.log(array);
+
+array.sort( function (a, b) {
+  return a-b;
+});
+
+console.log(array);
+
